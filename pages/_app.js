6582febe071e250f9ebf6ed2504b3bb26a457d2a
Karanpaul85 <69508163+Karanpaul85@users.noreply.github.com>
2023-06-19 10:18:16 +0530

    UPDATE Google analytics in GA4
    UPDATE Google analytics in GA4

--- pages/_app.js
+++ pages/_app.js
@@ -9,14 +9,14 @@ const App =  function App({ Component, pageProps }) {
    <>
       <Script
          strategy="lazyOnload"
-         src={`https://www.googletagmanager.com/gtag/js?id=UA-129726030-1`}
+         src={`https://www.googletagmanager.com/gtag/js?id=G-4V646WXZQ4`}
          />
          <Script strategy="lazyOnload">
          {`
                window.dataLayer = window.dataLayer || [];
                function gtag(){dataLayer.push(arguments);}
                gtag('js', new Date());
-               gtag('config', 'UA-129726030-1', {
+               gtag('config', 'G-4V646WXZQ4', {
                page_path: window.location.pathname,
                });
          `}
